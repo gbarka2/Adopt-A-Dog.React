f7a38e065ca947d737f20959e3c2ad5effe0eb2e
Gary Barkauski <gbarka2@gmail.com>
2021-02-22 19:57:36 -0700

    Add build folder

---
+++ build/precache-manifest.28745b3a73007185dce5a8b6daeb9011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e13277708daf0710e97eacfcad4c50d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0643765e573eb3addb80",
+    "url": "/static/css/2.6a7ab022.chunk.css"
+  },
+  {
+    "revision": "1242023c5a2b66adee51",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0643765e573eb3addb80",
+    "url": "/static/js/2.4d49a970.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4d49a970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1242023c5a2b66adee51",
+    "url": "/static/js/main.e9279f77.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
